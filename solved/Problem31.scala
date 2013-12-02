331df66858d043e462010c3d1239feca9c94b1c0
Alex Quach <alexhquach@gmail.com>
2013-12-02 00:23:16 -0800

    31 and 76.

---
+++ solved/Problem31.scala
@@ -0,0 +1,29 @@
+import scala.collection.mutable.Map
+
+object Project31 {
+  case class Memo[A, B](f: A => B) extends (A => B) {
+    private val cache = scala.collection.mutable.Map.empty[A, B]
+    def apply(x: A) = cache.getOrElseUpdate(x, f(x))
+  }
+
+  def main(args: Array[String]) {
+    val coins = Array[Int](1, 2, 5, 10, 20, 50, 100, 200)
+
+    def successor(x: Int) = coins(coins.indexOf(x) - 1)
+
+    lazy val numWays: Memo[(Int, Int), BigInt] = Memo {
+      case (0, _) => 1
+      case (_, 1) => 1
+      case (total, maxIntToUse) =>
+        numWays(total, successor(maxIntToUse)) + (
+          if (total >= maxIntToUse) {
+            numWays(total - maxIntToUse, maxIntToUse)
+          } else {
+            0
+          }
+        )
+    }
+
+    println(numWays(200, coins.last))
+  }
+}
