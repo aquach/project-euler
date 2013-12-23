cc29531ee04380fdb2cc869bcf00626d0f8b2996
Alex Quach <alexhquach@gmail.com>
2013-12-23 13:50:38 -0800

    Second pass, kind of.

--- Problem88.scala
+++ Problem88.scala
@@ -1,28 +1,39 @@
 object Problem88 {
-  def getAllProductMaps(x: Int, k: Int): Set[Map[Int, Int]] = 
-    if (k == 1) {
-      Set[Map[Int, Int]](Map(x -> 1))
-    } else {
-      (for (i <- (1 to x / 2).filter(x % _ == 0);
-           productMap <- getAllProductMaps(x / i, k - 1)
-         ) yield {
-         productMap.updated(i, productMap.get(i).getOrElse(0) + 1)
-       }).toSet
-    }
 
-  def isProductSum(x: Int, k: Int) =
-    !getAllProductMaps(x, k).filter(_.toIterator.map(Function.tupled(_ * _)).sum == x).isEmpty
+  def getPrimeFactorization(x: Int): List[Int] =
+    (2 to x / 2).foldLeft((List[Int](), x))((d, divisor) => {
+      val (factors, i) = d
+
+      if (i % divisor == 0) {
+        val s = Stream.iterate(i)(_ / divisor).takeWhile(_ % divisor == 0)
+        (
+          List.fill(s.size)(divisor) ++ factors,
+          s.last
+        )
+      } else {
+        d
+      }
+    })._1
+
+  def computeSums(factors: List[Int]): Set[Int] = {
+    def computeSums(factors: Vector[Int]): Vector[Int] = 
+      factors.sum +:
+        (for (
+          x <- (0 until factors.size);
+          rest = factors.patch(x, Nil, 1);
+          y <- (0 until rest.size)
+        ) yield {
+        println(x, y)
+          val newNum = factors(x) * rest(y)
+          computeSums(newNum +: rest.patch(y, Nil, 1))
+        }).flatten.toVector
+
+    computeSums(factors.toVector).toSet
+  }
 
   def main(args: Array[String]) {
-    println(
-      (for (
-        k <- (2 to 50).toIterator;
-        min = Stream.from(k).toIterator.dropWhile(i => !isProductSum(i, k)).next
-      ) yield {
-        println(k, min, getAllProductMaps(min, k).filter(_.toIterator.map(Function.tupled(_ * _)).sum == min))
-        min
-      }
-     ).toSet.sum
-    )
+    println(computeSums(getPrimeFactorization(60)))
+    //val lookup = (2 to 1000).map(((x: Int) => { println(x); x }) andThen getPrimeFactorization _ andThen computeSums _).toVector
+    //println(lookup)
   }
 }
